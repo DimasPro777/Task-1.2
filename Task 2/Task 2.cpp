0905a6a612096341ea4a3ef3018f3b80d31c6ed0
Дима <@DESKTOP-J3V34PA>
2022-09-09 05:17:41 +0300

    Добавьте файлы проекта.

---
+++ Task 2/Task 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    int b, c;
+    cout << "Введите переменные b и c: ";
+    cin >> b >> c;
+    if (b == 0)
+    {
+        if (c == 0)
+        {
+            cout << "x любое число";
+        }
+        else
+        {
+            cout << "Ошибка"
+        }
+    }
+    else
+    {
+        cout << "x = " << -c / b
+    }
+}
